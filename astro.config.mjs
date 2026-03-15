af141f0b8408591762476a31817619a37e0c019b
utkarsh348 <utkarsh348@gmail.com>
2026-03-15 19:33:56 +0530

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://utkarsh348.github.io',
-	base: '/PortfolioWebsite',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
